6d94c7572501d863f604c6fdfbbb769d336c79de
Ujjwal Sharma <sharmaujjwal5705@gmail.com>
2025-06-13 23:31:26 +0530

    Import

--- dataentry/management/commands/importdata.py
+++ dataentry/management/commands/importdata.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 from django.apps import apps
 import csv
+from django.db.utils import DataError
 
 # Proposed Command : pythonmanage.py importdata file_path model_name
 
@@ -21,12 +22,25 @@ class Command(BaseCommand):
             #search model in current app
             try:
                 model = app.get_model(model_name)
+                
+                # Getting field names of model
+                model_fields = [field.name for field in model._meta.fields]
+                model_fields = model_fields[1:]
+
                 with open(file_path, 'r') as file:
                     reader = csv.DictReader(file)
+                    csv_header = reader.fieldnames
+                    print(csv_header)
+                    print(model_fields)
+                    
+                    # Comparing CSV file header with model_fields
+                    if csv_header != model_fields:
+                        raise DataError(f"CSV File does not match with the {model.__name__} table fields")
+                    
                     for row in reader:
                         model.objects.create(**row)
                 self.stdout.write(self.style.SUCCESS('Data Imported from CSV file Successfully!'))
-                break   #Once model is found, import the data and break the loop
+                break    #Once model is found, import the data and break the loop
             except LookupError:
                 continue #Continue if model is not found
 
